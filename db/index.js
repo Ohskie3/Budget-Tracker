6863fbc050d1f0eae244d282c2cef0e32ca2e599
Kyle Ohlwiler <kohlwiler@yahoo.com>
2021-04-06 15:15:32 -0700

    fixed server issue

--- db/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', { useNewUrlParser: true, 
-useUnifiedTopology: true, 
-useFindAndModify: false 
-})
